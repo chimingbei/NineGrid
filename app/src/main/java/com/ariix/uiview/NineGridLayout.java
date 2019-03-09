c202f661434ddf925faa56882ed5c74f26f8267c
chimingbei <chimingbei@126.com>
2019-03-09 21:10:08 +0800

    Initial commit

---
+++ app/src/main/java/com/ariix/uiview/NineGridLayout.java
@@ -0,0 +1,15 @@
+package com.ariix.uiview;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class NineGridLayout extends ViewGroup {
+    public NineGridLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
